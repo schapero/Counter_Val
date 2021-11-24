5b8fe04e06d853d8a1da56b69e4970bb029ab838
Shubham gaur <gaur7699@gmail.com>
2021-11-24 23:13:24 +0530

    first one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import CounterValue from "./CounterValue";
+import Loader from "./Loader";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef(null);
+  const maxValue = process.env.REACT_APP_MAX_VALUE
+    ? process.env.REACT_APP_MAX_VALUE
+    : 1000;
+
+  const setCounterValue = (value) => {
+      setCounter(value);
+      setLoading(true);
+  
+      async function fetchData(){
+        try {
+          const counte = await fetch("https://interview-8e4c5-default-rtdb.firebaseio.com/front-end.json",{7318019088: +value});
+          setLoading (false);
+        } catch (error){
+          setLoading(false);
+          console.log(error);
+        }
+      };
+      fetchData();
+    }; 
+
+  const onInputChange = (event) => {
+    const target = event.nativeEvent.target;
+    const value = target.value;
+
+    if (isNaN(value) || +value > maxValue) return;
+
+    event.nativeEvent.target.setAttribute(
+      "size",
+      value.length !== 0 ? value.length : 1
+    );
+
+    if (value === "") setCounterValue(0);
+    else setCounterValue(value);
+  };
+
+  const onDecr = () => {
+    const currentCounter = +counter;
+
+    if (currentCounter - 1 > maxValue) return;
+    if (inputRef.current)
+      inputRef.current.setAttribute(
+        "size",
+        (currentCounter - 1).toString().length
+      );
+    setCounterValue((currentCounter - 1).toString());
+  };
+
+  const onIncrement = () => {
+    const currentCounter = +counter;
+
+    if (currentCounter + 1 > maxValue) return;
+    if (inputRef.current)
+      inputRef.current.setAttribute(
+        "size",
+        (currentCounter + 1).toString().length
+      );
+    setCounterValue((currentCounter + 1).toString());
+  };
+
+  useEffect(() => {
+    async function counteri(){
+      try{
+        const co = await fetch("https://interview-8e4c5-default-rtdb.firebaseio.com/front-end/7318019088.json")
+        const data = co.data;
+        if (data && data < maxValue) setCounter(data);
+        else setCounter(1);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    counteri();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screenContainer">
+      <div className="container">
+        {loading && (
+          <div className="loaderContainer">
+            <Loader />
+            <span className="loaderText">Saving counter value</span>
+          </div>
+        )}
+        <div className="counterContainer">
+          <div className="minus" onClick={onDecr}>
+            -
+          </div>
+          <div className="counter">
+            <input
+              className="counterInput"
+              ref={inputRef}
+              value={counter}
+              onChange={onInputChange}
+              size={counter.toString().length}
+            />
+          </div>
+          <div className="plus" onClick={onIncrement}>
+            +
+          </div>
+        </div>
+        <CounterValue value={counter} />
+      </div>
     </div>
   );
 }
---
+++ src/CounterValue.js
@@ -0,0 +1,7 @@
+import "./CounterValue.css";
+
+function CounterValue({ value }) {
+  return <div className="counterValue">Counter Value: {value}</div>;
+}
+
+export default CounterValue;
